7536c57e92183a2401e8cfd5c9f8b9bc55b72449
guibinfeng <guibinfeng@guibinfengdemac.local>
2016-07-20 13:00:20 +0900

    Initial Commit

---
+++ DYZB/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DYZB
+//
+//  Created by guibinfeng on 16/7/20.
+//  Copyright © 2016年 guibinfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DYZB/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DYZB
+//
+//  Created by guibinfeng on 16/7/20.
+//  Copyright © 2016年 guibinfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
